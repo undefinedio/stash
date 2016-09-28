ae736ce3a2cbff75976ebca950d7af59dc9da00f
Simon Peters <simon@unde.fined.io>
2016-09-28 10:28:54 +0200

    Auto backup on database push

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,7 @@ server 'server.stash.io', user: $user, roles: %w{web app db}
 set :deploy_to, -> { "/home/#$user/app" }
 set :tmp_dir, "/home/#$user/tmp"
 set :branch, :'master'
+set :wpcli_backup_db, true
 
 set :wpcli_remote_url, "#$url"
 set :wpcli_local_url, "dev.#$url"
