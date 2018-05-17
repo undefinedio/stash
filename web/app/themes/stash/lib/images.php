466bf0c30bdaceb24a4c062e23063721d23bee1d
Vincent Peters <me@vincentpeters.be>
2018-05-17 10:36:37 +0200

    Fixed ssl fetch images bug

--- web/app/themes/stash/lib/images.php
+++ web/app/themes/stash/lib/images.php
@@ -22,11 +22,18 @@ class ImageHelper
      */
     function display($name)
     {
+        $stream_opts = [
+           "ssl" => [
+               "verify_peer"      => false,
+               "verify_peer_name" => false,
+           ]
+       ];
+        
         $imageUrl = $this->get($name);
 
         //check if is an svg, if so embed svg code else display image element
         if (strpos($imageUrl, '.svg') !== false) {
-            return file_get_contents($imageUrl);
+            return file_get_contents($imageUrl, false, stream_context_create($stream_opts));
         } else {
             return "<img src='" . $this->get($name) . "' />";
         }
